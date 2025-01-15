db92c630301d1ea5945d68ff7acaaad5356584eb
Lisandro V <lisandrovazquez@gmail.com>
2025-01-14 22:26:56 -0300

    add ansible for internal server conf

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,9 +17,51 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
+data "aws_vpc" "default" {
+  default = true
+}
+
+resource "aws_security_group" "web_sg" {
+  name        = "web_sg"
+  description = "Allow inbound traffic on ports 22, 3000, and 3001"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    description = "Allow SSH traffic on port 22"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow HTTP traffic on port 3000"
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow HTTP traffic on port 3001"
+    from_port   = 3001
+    to_port     = 3001
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.tipo_instancia
-  key_name      = "codigo-facilito" 
+  key_name      = "codigo-facilito"
   tags          = local.common_tags
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
 }
